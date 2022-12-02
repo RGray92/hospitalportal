481156e2bdd717b20150a6ea395141c193fbc0ca
RGray92 <ryandgray92@gmail.com>
2022-12-02 10:36:04 +0000

    updating folder structure

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import React from 'react';
+// import { Component } from 'react';
+import Login from '../components/Login';
+// import Register from '../components/Register';
+
+const Home = () => {
+      const [openLoginModal, setLoginModal] = useState(false);
+
+      return (
+          <>
+      <div className="container">
+        <div className="banner-text">
+          <h1>WELCOME TO CLYDE CHILDRENS HOSPITAL</h1>
+          <p>LOGIN OR REGISTER TO ENTER THE HOSPITAL PORTAL</p>
+        </div>
+        <div className="btns">
+          <button className="btn login-btn" onClick={() => {setLoginModal(true);}}>Login</button>
+          <button className="btn signup-btn">Register</button>
+        </div>
+        {/* signup container */}
+        {/* <Register /> */}
+       
+          {/* login container */}
+          {openLoginModal && <Login />}
+        </div>
+          </>
+      );
+    }
+
+export default Home;
